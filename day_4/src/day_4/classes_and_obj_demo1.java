685fa85c4d9a71bad175d912902d9df74a4553a7
google <google@DESKTOP-120RTMA>
2022-06-10 16:24:16 +0530

    first commit

---
+++ day_4/src/day_4/classes_and_obj_demo1.java
@@ -0,0 +1,15 @@
+package day_4;
+
+public class classes_and_obj_demo1 {
+int x;
+}
+class testdemo{
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+classes_and_obj_demo1 myobj = new classes_and_obj_demo1();
+myobj.x=40;
+System.out.println(myobj.x);
+	}
+	}
+
+
---
+++ day_4/src/day_4/classes_and_obj_demo2.java
@@ -0,0 +1,15 @@
+package day_4;
+
+public class classes_and_obj_demo2 {
+int x;
+	public static void main(String[] args) {
+classes_and_obj_demo2 myobj1 = new classes_and_obj_demo2();
+classes_and_obj_demo2 myobj2 = new classes_and_obj_demo2();
+myobj1.x = 24;
+myobj2.x = 55;
+System.out.println(myobj1.x);
+System.out.println(myobj2.x);
+
+	}
+
+}
