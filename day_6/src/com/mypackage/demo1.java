9472dd2c024dfd1766c53d4f7cef883c4b222799
User <User@LAPTOP-K6UES4VI>
2022-06-12 13:48:09 +0530

    first commit

---
+++ day_6/src/com/mypackage/demo1.java
@@ -0,0 +1,11 @@
+package com.mypackage;
+
+public class demo1 {
+public int i=10;
+public void setname(String name) {
+	System.out.println(name);
+
+
+	}
+
+}
---
+++ day_6/src/com/mypackage1/demo2.java
@@ -0,0 +1,14 @@
+package com.mypackage1;
+import com.mypackage.demo1;
+import com.mypackage.demo1;
+public class demo2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+String name ="welcome to c2tc program";
+demo1 d1=new demo1();
+d1.setname(name);
+System.out.println(d1.i);
+	}
+
+}
