0fb9cb5c6f53d63af94be1aaba026d004d42de16
User <User@LAPTOP-K6UES4VI>
2022-06-15 16:57:39 +0530

    first commit

---
+++ day_10/src/Wrapper/shift_operator.java
@@ -0,0 +1,11 @@
+package Wrapper;
+
+public class shift_operator {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int c=100;
+int d=c>>3;
+System.out.println(d);
+}
+}//understood the operation of shift operator
