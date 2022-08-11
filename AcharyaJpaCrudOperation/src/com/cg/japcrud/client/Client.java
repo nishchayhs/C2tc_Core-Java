76ed64af512ca140c12d072d0820f6242a2f9efe
User <User@LAPTOP-K6UES4VI>
2022-08-11 11:18:26 +0530

    FIRSTCOMMIT

---
+++ AcharyaJpaCrudOperation/src/com/cg/japcrud/client/Client.java
@@ -0,0 +1,37 @@
+package com.cg.japcrud.client;
+import com.cg.jpacrud.entity.Student;
+import com.cg.jpacrud.service.StudentService;
+import com.cg.jpacrud.service.StudentServiceImpl;
+
+public class Client {
+
+	public static void main(String[] args) {
+
+		Student student = new Student();
+		StudentService service = new StudentServiceImpl();
+		
+		student.setStudentId(100);
+		student.setName("NISHCHAY");
+		service.addStudent(student);
+		
+		student = service.findStudentById(100);
+		System.out.print("ID:"+student.getStudentId());
+		System.out.println(" Name:"+student.getName());
+		
+		student = service.findStudentById(100);
+		student.setName("NISHCHAY NAVNNETH");
+		service.updateStudent(student);
+		
+		student = service.findStudentById(100);
+		System.out.print("ID:"+student.getStudentId());
+		System.out.println(" Name:"+student.getName());
+		
+		student = service.findStudentById(100);
+		service.removeStudent(student);
+		System.out.println("End of program");
+
+		
+
+	}
+
+}
---
+++ AcharyaJpaCrudOperation/src/com/cg/jpacrud/service/StudentServiceImpl.java
@@ -0,0 +1,46 @@
+package com.cg.jpacrud.service;
+
+import com.cg.jpacrud.dao.StudentDaoImpl;
+import com.cg.jpacrud.dao.Studentdao;
+import com.cg.jpacrud.entity.Student;
+
+public class StudentServiceImpl implements StudentService {
+	
+    private Studentdao dao;
+	
+	public StudentServiceImpl()
+	{
+		dao = new StudentDaoImpl();
+	}
+
+	@Override
+	public void addStudent(Student student) {
+		dao.beginTransaction();
+		dao.addStudent(student);
+		dao.commitTransaction();
+		
+	}
+
+	@Override
+	public void updateStudent(Student student) {
+		dao.beginTransaction();
+		dao.updateStudent(student);
+		dao.commitTransaction();
+		
+	}
+
+	@Override
+	public void removeStudent(Student student) {
+		dao.beginTransaction();
+		dao.removeStudent(student);
+		dao.commitTransaction();
+		
+	}
+
+	@Override
+	public Student findStudentById(int id) {
+		Student student = dao.getStudentById(id);
+		return student;
+	}
+
+}
